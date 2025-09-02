1274fe72db98395bffa87f896cc31b41a7f99fa5
lmy <mengyaliu6013@163.com>
2025-09-02 18:13:19 +0800

    step01 修改测试

--- tests/teststep01.py
+++ tests/teststep01.py
@@ -5,9 +5,9 @@ import numpy as np
 class TestStep01(unittest.TestCase):
 
     def test_variable(self):
-        data=np.array(7)
+        data=np.array([7,8,9])
         v=Variable(data)
-        self.assertEqual(v.data,7)
+        self.assertEqual(v.data.ndim,1)
 
 if __name__=='__main__':
     unittest.main()
